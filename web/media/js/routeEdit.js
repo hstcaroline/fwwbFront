0de8919edef50eb6cbe2d748a698f3573016c4b6
hst <706491433@qq.com>
2016-07-15 15:47:06 +0800

    拖拽

--- web/media/js/routeEdit.js
+++ web/media/js/routeEdit.js
@@ -1,13 +1,121 @@
-var blocks=document.getElementById("blocks");
-var block_data=[3,3,3,3,3,3,3,3,3];
-var COLOR=["#FF9900","#333333","#548C00","##009933","#CC0066","#009999","#666699","#FF6600","#8F4586"];
-init();
-function init() {
-    for(var i=0;i<block_data.length;i++){
-        for(var j=0;j<block_data[i];j++){
-            var row=j+1;
-            var col=i+1;
-            blocks.innerHTML+="<li data-row='"+row+"' data-col='"+col+"' data-sizex='1' data-sizey='1' style='background:"+COLOR[i%COLOR.length]+" '> 车站名称</li>";
+//var blocks = document.getElementById("blocks");
+var routs = [{
+    'id': 'id1',
+    'name': '线路1',
+    'stations': [{
+        'id': 1,
+        'name': '站点1',
+    }, {
+        'id': 2,
+        'name': '站点2',
+    }]
+}
+    , {
+        'id': 2,
+        'name': '线路2',
+        'stations': [{
+            'id': 3,
+            'name': '站点3',
+        }, {
+            'id': 4,
+            'name': '站点4',
+        }]
+    }, {
+        'id': 3,
+        'name': '线路3',
+        'stations': [{
+            'id': 5,
+            'name': '站点5',
+        }, {
+            'id': 6,
+            'name': '站点6',
+        }]
+    }];
+
+//var routs = [{
+//    "route": {"id": 1, "name": "route", "time": null, "status": "ok"},
+//    "stations": [{
+//        "id": 1,
+//        "name": "station",
+//        "address": "station",
+//        "posx": 123,
+//        "posy": 123,
+//        "time": "2016-07-18T22:16:24.000Z"
+//    }]
+//}];
+//var block_data = [14, 13, 13, 3, 3, 13, 3, 15, 6, 7, 8, 9, 14, 17];
+var COLOR = ["#FF9900", "#333333", "#548C00", "##009933", "#CC0066", "#009999", "#666699", "#FF6600", "#8F4586"];
+var stations = [];
+loadData(routs);
+//function init() {
+//    for (var i = 0; i < block_data.length; i++) {
+//        for (var j = 0; j < block_data[i]; j++) {
+//            var row = j + 1;
+//            var col = i + 1;
+//            blocks.innerHTML += "<li data-row='" + row + "' data-col='" + col + "' data-sizex='1' data-sizey='1' style='background:" + COLOR[i % COLOR.length] + " '> 车站名称</li>";
+//        }
+//    }
+//}
+
+function loadData(routs) {
+
+    for (var i = 0; i < routs.length; i++) {
+        for (var j = 0; j < routs[i].stations.length; j++) {
+            var row = j + 1;
+            var col = i + 1;
+            stations.push(routs[i].stations[j]);
+            blocks.innerHTML += "<li data-row='" + row + "' data-col='" + col + "' data-sizex='1' data-sizey='1' style='background:" + COLOR[i % COLOR.length] + " '> " + "<span style='display: none' class='id'>" + routs[i].stations[j].id + "</span>" + routs[i].stations[j].name + "</li>";
         }
     }
+    render();
+}
+function render() {
+    var gridster;
+    $(function () {
+        gridster = $(".gridster > ul").gridster({
+            widget_margins: [15, 10],
+            widget_base_dimensions: [75, 25],
+            min_cols: 6
+        }).data('gridster');
+    });
+
+}
+var newRoutes = routs;
+function saveRoute() {
+    //var lis = $("#blocks li");
+    //var liLength = lis.length;
+    //alert(liLength);
+
+    for (var i = 0; i < newRoutes.length; i++) {
+        newRoutes[i].length = 0;
+    }
+    var col = 1;
+    $("#blocks li").each(function () {
+
+        var data_row = $(this).attr('data-row') - 1;
+        var data_col = $(this).attr('data-col') - 1;
+
+        var currentId = $(this).children('span').text();
+        for (var j = 0; j < stations.length; j++) {
+            if (currentId == stations[j].id) {
+                newRoutes[data_col].stations[data_row] = stations[j];
+            }
+        }
+        //if(data_row>newRoutes[data_col].stations.length){
+        //    for(var j=0;j<stations.length;j++){
+        //
+        //
+        //    }
+        //   // newRoutes[data_col].stations.push()
+        //}
+        //alert(currentId);
+        //newRoutes[data_col].stations[data_row].id = $(this).children('span').innerHTML;
+        //i++;
+    });
+    //routs = newRoutes;
+    //$("#blocks").empty();
+    //$("#blocks").attr('style', null);
+    //$("#grid").attr('class', 'gridster');
+    loadData(newRoutes);
+    // render();
 }
