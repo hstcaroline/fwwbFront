4def5b6de48ac9685f53d43b5d3030534ba84403
Philic <xlw125414713@163.com>
2016-07-15 15:41:03 +0800

    fix some bug

--- driverApp/www/js/index.js
+++ driverApp/www/js/index.js
@@ -1,3 +1,12 @@
+function showStaffInfo() {
+    $("#readCardBtn").hide();
+    $("#readCard").animate({
+        paddingTop: '5%',
+        paddingBottom: '5%',
+    });
+    $("#staffInfo").hide();
+    $("#staffInfo").slideDown("slow");
+}
 var app = {
     initialize: function () {
         $("#scan_info").val("app ready");
@@ -40,20 +49,12 @@ var app = {
                 failure
             );
         }
-        app.compileTemplates();
-        app.addTemplateHelpers();
-        app.showInstructions();
     },
     onNfc: function (nfcEvent) {
-
         var tag = nfcEvent.tag;
         var temInfo = JSON.stringify(tag);
         $("#scan_info").val("onNfc "+temInfo);
-        console.log(JSON.stringify(nfcEvent.tag));
-        app.clearScreen();
-
-        tagContents.innerHTML = app.nonNdefTagTemplate(tag);
-        navigator.notification.vibrate(100);
+        showStaffInfo();
     },
     onNdef: function (nfcEvent) {
         var tag = nfcEvent.tag;
@@ -65,128 +66,5 @@ var app = {
             tag.isWritable = !tag.isLocked;
             tag.canMakeReadOnly = tag.isLockable;
         }
-
-        tagContents.innerHTML = app.tagTemplate(tag);
-
-        navigator.notification.vibrate(100);
-    },
-    clearScreen: function () {
-
-        tagContents.innerHTML = "";
-
-    },
-    showInstructions: function () {
-
-        var hidden = document.getElementsByClassName('hidden');
-        if (hidden && hidden.length) {
-            hidden[0].className = 'instructions';
-        }
-
-    },
-    compileTemplates: function () {
-
-        var source;
-
-        source = document.getElementById('non-ndef-template').innerHTML;
-        app.nonNdefTagTemplate = Handlebars.compile(source);
-
-        source = document.getElementById('tag-template').innerHTML;
-        app.tagTemplate = Handlebars.compile(source);
-
-    },
-    addTemplateHelpers: function () {
-
-        Handlebars.registerHelper('bytesToString', function(byteArray) {
-            return nfc.bytesToString(byteArray);
-        });
-
-        Handlebars.registerHelper('bytesToHexString', function(byteArray) {
-            return nfc.bytesToHexString(byteArray);
-        });
-
-        // useful for boolean
-        Handlebars.registerHelper('toString', function(value) {
-            return String(value);
-        });
-
-        Handlebars.registerHelper('tnfToString', function(tnf) {
-            return tnfToString(tnf);
-        });
-
-        Handlebars.registerHelper('decodePayload', function(record) {
-            return decodePayload(record);
-        });
-
-        Handlebars.registerHelper('pluralize', function(number, single, plural) {
-          if (number === 1) { return single; }
-          else { return plural; }
-        });
     }
 };
-
-// ideally some form of this can move to phonegap-nfc util
-function decodePayload(record) {
-    var recordType = nfc.bytesToString(record.type),
-        payload;
-
-    // TODO extract this out to decoders that live in NFC code
-    // TODO add a method to ndefRecord so the helper
-    // TODO doesn't need to do this
-
-    if (recordType === "T") {
-        var langCodeLength = record.payload[0],
-        text = record.payload.slice((1 + langCodeLength), record.payload.length);
-        payload = nfc.bytesToString(text);
-
-    } else if (recordType === "U") {
-        var identifierCode = record.payload.shift(),
-        uri =  nfc.bytesToString(record.payload);
-
-        if (identifierCode !== 0) {
-            // TODO decode based on URI Record Type Definition
-            console.log("WARNING: uri needs to be decoded");
-        }
-        //payload = "<a href='" + uri + "'>" + uri + "<\/a>";
-        payload = uri;
-
-    } else {
-
-        // kludge assume we can treat as String
-        payload = nfc.bytesToString(record.payload);
-    }
-
-    return payload;
-}
-
-// TODO move to phonegap-nfc util
-function tnfToString(tnf) {
-    var value = tnf;
-
-    switch (tnf) {
-    case ndef.TNF_EMPTY:
-        value = "Empty";
-        break;
-    case ndef.TNF_WELL_KNOWN:
-        value = "Well Known";
-        break;
-    case ndef.TNF_MIME_MEDIA:
-        value = "Mime Media";
-        break;
-    case ndef.TNF_ABSOLUTE_URI:
-        value = "Absolute URI";
-        break;
-    case ndef.TNF_EXTERNAL_TYPE:
-        value = "External";
-        break;
-    case ndef.TNF_UNKNOWN:
-        value = "Unknown";
-        break;
-    case ndef.TNF_UNCHANGED:
-        value = "Unchanged";
-        break;
-    case ndef.TNF_RESERVED:
-        value = "Reserved";
-        break;
-    }
-    return value;
-}
