34a273253260ab7453c7eeb161edc18228734a67
hst <706491433@qq.com>
2016-07-23 14:41:08 +0800

    添加我的车辆信息

--- driverApp/www/js/myJs.js
+++ driverApp/www/js/myJs.js
@@ -1,19 +1,25 @@
 /**
  * Created by hst on 16/7/7.
  */
-function footerClick(id){
-    if(id=="footer_a1") {
-        location.href="index.html";
+function footerClick(id) {
+    if (id == "footer_a1") {
+        location.href = "index.html";
     }
-    if(id=="footer_a2"){
-        location.href="mySchedule.html";
+    if (id == "footer_a2") {
+        location.href = "mySchedule.html";
     }
-    if(id=="footer_a3"){
-        location.href="nfcScan.html";
+    if (id == "footer_a3") {
+        location.href = "nfcScan.html";
     }
-
 }
-
-$("#myInfo").click(function(){
-    location.href="myInfo.html";
-});
+function menuClick(id) {
+    if (id == 'myInfo') {
+        location.href = 'myInfo.html';
+    }
+    if (id == 'myMsg') {
+        location.href = 'myMsg.html';
+    }
+    if(id=='myCar'){
+        location.href = 'myCar.html';
+    }
+}
