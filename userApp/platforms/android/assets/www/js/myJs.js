cd06dd85d99354de906c4d4573705c735904c21c
Ce Gao <ce.gao@outlook.com>
2016-08-01 22:49:55 +0800

    add user app

---
+++ userApp/platforms/android/assets/www/js/myJs.js
@@ -0,0 +1,26 @@
+/**
+ * Created by hst on 16/7/7.
+ */
+function footerClick(id){
+    if(id=="footer_a1") {
+        location.href="index.html";
+    }
+    if(id=="footer_a2"){
+        location.href="myInfo.html";
+    }
+    if(id=="footer_a3"){
+        location.href="chargeForFun.html";
+    }
+
+}
+function menuClick(id) {
+    if (id == 'myWork') {
+        location.href = 'myWork.html';
+    }
+    if (id == 'myMsg') {
+        location.href = 'myMsg.html';
+    }
+    if(id=='news'){
+        location.href = 'news.html';
+    }
+}
